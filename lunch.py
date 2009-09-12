23a29df7dce990ccc1a62c8a52edb0b6a203bbb4
Marco Garcês <marco@garces.cc>
2009-09-12 19:50:48 +0100

    changed input method, now everything in one line.

--- lunch.py
+++ lunch.py
@@ -1,31 +1,35 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+#Marco Garcês - marco at garces.cc
+ 
 #open txt and parse it to a list (dinner or lunch); returns the menu, clean without \n
 import sys, os
-
+ 
 def open_menu(choice):
 	if choice == 'almoço':
-		file = 'input/lunch.txt'
+		file = './input/lunch.txt'
 	elif choice == 'jantar':
-		file = 'input/dinner.txt'
+		file = './input/dinner.txt'
 	else:
 		print 'escolha não definida/encontrada\n'
 		sys.exit()
+	
 	f = open(file, 'r')
 	linhas = f.readlines()
 	f.close()
-	
 	#clean lines from \n
+	
 	menu = []
 	for item in linhas:
 		menu.append(str(item).strip())
 	return menu
-	
-
+ 
+ 
 #find and output desired menu
 def get_meal(choice, date):
+	
 	menu = open_menu(choice)
+	
 	if date in menu:
 		n = menu.index(date)
 		if choice == 'almoço':
@@ -38,26 +42,13 @@ def get_meal(choice, date):
 			menu_day.insert(0, choice)
 			return '\n Menu %s %s: sopa de %s, %s, %s\n Sobremesa: %s\n' \
 			%(menu_day[0], menu_day[1], menu_day[2], menu_day[3], menu_day[4], menu_day[5])
-	
+ 
 	else:
 		return 'Menu não se encontra disponível\n para este dia \n'
-
-def clearscreen(numlines=100):
-	# Clear the console.
-	# import os
-	if os.name == "posix":
-		# Unix/Linux/MacOS/BSD/etc
-		os.system('clear')
-	elif os.name in ("nt", "dos", "ce"):
-		# DOS/Windows
-		os.system('CLS')
-	else:
-		# Fallback for other operating systems.
-		print '\n' * numlines
-
-
-
-choice = raw_input("deseja saber o menu do almoço ou do jantar? \n")
-date = raw_input("qual o dia do menu que quer?(de 1-09-2009 a 30-09-2009) \n")
-
+ 
+ 
+ 
+user_input = raw_input().split()
+choice = user_input[0]
+date = user_input[1]
 print get_meal(choice, date)
