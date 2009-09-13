009c1e60d3a177ccaefcd6a09a636a1fccc7030e
Marco Garcês <marco@garces.cc>
2009-09-13 05:55:59 +0100

    del and get DM tested and working. UTF-8 bug

--- infoUAlg.py
+++ infoUAlg.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 #Marco Garcês - marco at garces.cc
  
-import sys, os, twitter, settings
+import sys,os,twitter,settings
+#twitter api provided by http://code.google.com/p/python-twitter/
 
 #open txt and parse it to a list (dinner or lunch); returns the menu, clean without \n
 def open_menu(choice):
@@ -46,7 +47,11 @@ def get_meal(choice, date):
 	else:
 		return 'Menu não se encontra disponível\n para este dia \n'
 
-def get_twiiter_dm():
+def del_twitter_dm(dm_id):
+	# api = twitter.Api(username=settings.TWITTER_USER,passwd=settings.TWITTER_PASSWD) testing this
+	api.DestroyDirectMessage(id=dm_id)
+
+def get_twitter_dm():
 	api = twitter.Api(username=settings.TWITTER_USER,password=settings.TWITTER_PASSWD)
 	dm_messages = api.GetDirectMessages()
 	dm_input = dm_messages[0].text #string to use in get_meal()
@@ -56,20 +61,15 @@ def get_twiiter_dm():
 	dm_id = dm_messages[0].id #id number to delete DM after reply
 	dm_user = dm_messages[0].sender_screen_name #twitter user to reply answer
 	del_twitter_dm(dm_id)
-
-def del_twitter_dm(dm_id):
-	# api = twitter.Api(username=settings.TWITTER_USER,passwd=settings.TWITTER_PASSWD) testing this
-	api.api.DestroyDirectMessage(id=dm_id)
 	
-	
+#USER INPUT IS the string the user enters 
+user_input = get_twitter_dm().split()
 
-#user input is the string the user enters 
-user_input = get_twiiter_dm().split()
 #at the moment this is not implemented, ignore for now
-service = user_input[0]
+service = user_input[0].encode("utf-8")
 #at this moment, "choice" is what type of menu the user wants (jantar ou almoço)
-choice = user_input[1]
+choice = user_input[1].encode("utf-8")
 #date is self explanatory
-date = user_input[2]
+date = user_input[2].encode("utf-8")
 
 print get_meal(choice, date)
